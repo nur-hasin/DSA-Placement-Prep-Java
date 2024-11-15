76986b0d22db652ee8399e1ef3c53ee2df9c16e7
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2024-11-16 05:27:49 +0600

    Delete 1. Variables & Data Types directory

--- 1. Variables & Data Types/SumCalc.java
+++
@@ -1,8 +0,0 @@
-public class SumCalc {
-    public static void main(String args[]) {
-        int a = 10;
-        int b = 5;
-        int sum = (a + b);
-        System.out.print(sum);
-        } 
-}
