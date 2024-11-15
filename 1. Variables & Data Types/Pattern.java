71361a78a1529781e8fc63d6a59f885d507b3d4d
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2024-11-16 04:39:18 +0600

    Add files via upload

---
+++ 1. Variables & Data Types/Pattern.java
@@ -0,0 +1,8 @@
+public class Pattern {
+    public static void main (String arg[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
