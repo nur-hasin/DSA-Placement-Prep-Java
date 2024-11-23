209428580b6aa92affda8fe3791e9630a402a434
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2024-11-24 05:04:31 +0600

    SumCalcUsrIn.java

--- 1. Variables & Data Types/SumCalcUsrIn.java
+++ 1. Variables & Data Types/SumCalcUsrIn.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class SumCalcIn {
+public class SumCalcUsrIn {
     public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
